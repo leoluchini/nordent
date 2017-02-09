7f412aec84ad7dc484965cd364689e2eb54627e3
Leo Luchini <leonel.luchini@ec.gba.gov.ar>
2017-02-09 11:10:14 -0300

    active automatico navegador, cambios de textos en tratamientos

--- js/nordent.js
+++ js/nordent.js
@@ -24,6 +24,23 @@ $(function(){
   		$("html, body").animate({scrollTop: 0}, 1000);
 	});
 
+	$(window).scroll(function() {
+	    var windscroll = $(window).scrollTop();
+	    if (windscroll >= 100) {
+	        $('section').each(function(i) {
+	            if ($(this).position().top <= windscroll + 20) {
+	                $('nav li.active').removeClass('active');
+	                $('nav a').eq(i).parent().addClass('active');
+	            }
+	        });
+
+	    } else {
+	        $('nav a.active').removeClass('active');
+	        $('nav a:first').addClass('active');
+	    }
+
+	}).scroll();
+
 	$('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
