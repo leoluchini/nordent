4155d68203ed8ec3b280487032f15f965f0d8ef9
Leo Luchini <leonel.luchini@ec.gba.gov.ar>
2017-09-21 10:36:25 -0300

    arreglos de css y textos

--- js/nordent.js
+++ js/nordent.js
@@ -2,16 +2,16 @@ $(function(){
 
     $("img.lazy").lazyload();
 
-	window.sr = ScrollReveal();
-	
-	var hero = {
+    window.sr = ScrollReveal();
+    
+    var hero = {
         origin   : "top",
         distance : "24px",
         duration : 1500,
         scale    : 1.05
     };
 
-	var intro = {
+    var intro = {
         origin   : "bottom",
         distance : "64px",
         duration : 900,
@@ -19,32 +19,32 @@ $(function(){
         scale    : 1
     };
 
-	sr.reveal('.parallax-window > .container', intro);
+    sr.reveal('.parallax-window > .container', intro);
 
-	sr.reveal('.box', hero);
+    sr.reveal('.box', hero);
 
-	$(".toTop").click(function () {
-  		$("html, body").animate({scrollTop: 0}, 1000);
-	});
+    $(".toTop").click(function () {
+        $("html, body").animate({scrollTop: 0}, 1000);
+    });
 
-	$(window).scroll(function() {
-	    var windscroll = $(window).scrollTop();
-	    if (windscroll >= 100) {
-	        $('section').each(function(i) {
-	            if ($(this).position().top <= windscroll + 20) {
-	                $('nav li.active').removeClass('active');
-	                $('nav a').eq(i).parent().addClass('active');
-	            }
-	        });
+    $(window).scroll(function() {
+        var windscroll = $(window).scrollTop();
+        if (windscroll >= 100) {
+            $('section').each(function(i) {
+                if ($(this).position().top <= windscroll + 20) {
+                    $('nav li.active').removeClass('active');
+                    $('nav a').eq(i).parent().addClass('active');
+                }
+            });
 
-	    } else {
-	        $('nav a.active').removeClass('active');
-	        $('nav a:first').addClass('active');
-	    }
+        } else {
+            $('nav a.active').removeClass('active');
+            $('nav a:first').addClass('active');
+        }
 
-	}).scroll();
+    }).scroll();
 
-	$('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top - 15
@@ -52,22 +52,22 @@ $(function(){
         event.preventDefault();
     });
 
-	$( ".nordent-staff" ).mouseenter( function(){
-		$(this).find(".blackAndWhite").hide();
-		$(this).find(".color").show();
-		$('#nameTeam').html($(this).data('names'));
-		$('#whatHeDoTeam').html($(this).data('whathedo'));
-	});
+    $( ".nordent-staff" ).mouseenter( function(){
+        $(this).find(".blackAndWhite").hide();
+        $(this).find(".color").show();
+        $('#nameTeam').html($(this).data('names'));
+        $('#whatHeDoTeam').html($(this).data('whathedo'));
+    });
 
-	$( ".nordent-staff" ).mouseleave( function(){
-		$(this).find(".color").hide();
-		$(this).find(".blackAndWhite").show();
-		document.getElementById("nameTeam").innerHTML = " ";
-		document.getElementById("whatHeDoTeam").innerHTML = " ";
+    $( ".nordent-staff" ).mouseleave( function(){
+        $(this).find(".color").hide();
+        $(this).find(".blackAndWhite").show();
+        document.getElementById("nameTeam").innerHTML = " ";
+        document.getElementById("whatHeDoTeam").innerHTML = " ";
 
-	});
+    });
 
-	$('.parallax-window .mini-parallax-window').parallax({
-		speed: 0.3	
-	})
+    $('.parallax-window .mini-parallax-window').parallax({
+        speed: 0.3  
+    })
 })
